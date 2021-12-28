290a2d003cebfdc9b00a45fa909be7dea9220c61
AC0727 <adele.chen@ashbury.ca>
2021-12-27 23:26:05 -0500

    created movie class in models

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    #genre =
+    user_rating = models.DecimalField(max_digits=3, decimal_places=1)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
