f8256d966cdf1fc4d158d23e2f87e9b66e8cea29
AC0727 <adele.chen@ashbury.ca>
2021-12-28 21:00:28 -0500

    registered models to admin.py

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Movie, Watched, Genre
+
+admin.site.register(Movie)
+admin.site.register(Watched)
+admin.site.register(Genre)
+
