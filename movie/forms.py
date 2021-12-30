fed6a6b9a22e818f3ba0b525c7e6edbfc3d14123
AC0727 <adele.chen@ashbury.ca>
2021-12-29 23:28:59 -0500

    Changed add and edit forms review section from CharField to DecimalField

--- movie/forms.py
+++ movie/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    search = forms.CharField(label='Search')
+    search = forms.CharField(label='Add a movie')
 
 class AddForm(forms.Form):
-    rating = forms.CharField(label='Rating')
+    rating = forms.DecimalField(label='Rating')
     date = forms.DateField(label='Date')
     review = forms.CharField(label='Review')
 
 class EditForm(forms.Form):
-    rating = forms.CharField(label='Rating')
+    rating = forms.DecimalField(label='Rating')
     date = forms.DateField(label='Date')
     review = forms.CharField(label='Review')
 
